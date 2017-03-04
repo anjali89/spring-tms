517d6d4b693015807579177cd4f8663cd90991c2
Yash Ugrankar <yash.ugrankar@gmail.com>
2017-03-05 05:24:56 +0530

    Complete InsertVehicle functionality

--- sape-tms-proj/src/com/sapient/tms/controller/db/vehicle/InsertVehicleServlet.java
+++ sape-tms-proj/src/com/sapient/tms/controller/db/vehicle/InsertVehicleServlet.java
@@ -1,37 +1,67 @@
 package com.sapient.tms.controller.db.vehicle;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.sapient.tms.helper.VehicleData;
+import com.sapient.tms.model.bean.Vehicle;
+import com.sapient.tms.model.bl.VehicleLogic;
 
 /**
  * Servlet implementation class InsertVehicleServlet
  */
 public class InsertVehicleServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public InsertVehicleServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+	private VehicleLogic vehicleLogic;
+	private VehicleData vehicleData;
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	public InsertVehicleServlet() {
+		super();
+		vehicleData = new VehicleData();
+		vehicleLogic = new VehicleLogic();
+		// TODO Auto-generated constructor stub
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		try {
+			PrintWriter pw = response.getWriter();
+			boolean check = false;
+			vehicleData.createVehicle(request);
+			Vehicle vehicle = vehicleData.getVehicle();
+			check = vehicleLogic.insert(vehicle);
+			if (check) {
+				request.getRequestDispatcher("./db/vehicle/InsertVehicleSuccessful.jsp").forward(request, response);
+			} else {
+				request.setAttribute("err", "Operation Failed");
+				request.getRequestDispatcher("./db/vehicle/InsertVehicleFailed.jsp").forward(request, response);
+			}
+		}
+		catch (ClassNotFoundException | SQLException e) {
+			request.setAttribute("err", e.toString());
+			request.getRequestDispatcher("./InsertVehicleFailed.jsp").forward(request, response);
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
