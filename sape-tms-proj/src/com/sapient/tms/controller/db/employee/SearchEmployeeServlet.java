14c6af72563d3abd7ea13f4af6e19127eaef12e0
50 <50@noida>
2017-03-05 00:13:18 -0800

    Search Employee Done

--- sape-tms-proj/src/com/sapient/tms/controller/db/employee/SearchEmployeeServlet.java
+++ sape-tms-proj/src/com/sapient/tms/controller/db/employee/SearchEmployeeServlet.java
@@ -1,37 +1,66 @@
 package com.sapient.tms.controller.db.employee;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.sapient.tms.model.bean.Employee;
+import com.sapient.tms.model.bean.Vehicle;
+import com.sapient.tms.model.bl.EmployeeLogic;
+import com.sapient.tms.model.bl.VehicleLogic;
 
 /**
  * Servlet implementation class SearchEmployeeServlet
  */
 public class SearchEmployeeServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public SearchEmployeeServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+	private EmployeeLogic employeeLogic;
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	public SearchEmployeeServlet() {
+		super();
+		employeeLogic = new EmployeeLogic();
+		// TODO Auto-generated constructor stub
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		try {
+			PrintWriter pw = response.getWriter();
+			int employeeId = Integer.parseInt("id");
+			Employee employee = employeeLogic.search(employeeId);
+			if (employee != null) {
+				request.setAttribute("employee", employee);
+				request.getRequestDispatcher("./db/employee/SearchEmployeeSuccessful.jsp").forward(request, response);
+			} else {
+				request.setAttribute("err", "Entry Not Found");
+				request.getRequestDispatcher("./db/employee/SearchEmployeeFailed.jsp").forward(request, response);
+			}
+		}
+
+		catch (ClassNotFoundException | SQLException e) {
+			request.setAttribute("err", e.toString());
+			request.getRequestDispatcher("./SearchEmployeeFailed.jsp").forward(request, response);
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
