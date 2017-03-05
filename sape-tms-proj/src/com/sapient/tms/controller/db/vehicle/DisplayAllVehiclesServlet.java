bfc2f5d2749884349cea33e309ab98db4ab2d2c8
Yash Ugrankar <yash.ugrankar@gmail.com>
2017-03-05 13:54:54 +0530

    Complete DisplayAllServlets

--- sape-tms-proj/src/com/sapient/tms/controller/db/vehicle/DisplayAllVehiclesServlet.java
+++ sape-tms-proj/src/com/sapient/tms/controller/db/vehicle/DisplayAllVehiclesServlet.java
@@ -1,22 +1,31 @@
 package com.sapient.tms.controller.db.vehicle;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.sapient.tms.model.bean.Vehicle;
+import com.sapient.tms.model.bl.VehicleLogic;
 
 /**
  * Servlet implementation class DisplayAllVehiclesServlet
  */
 public class DisplayAllVehiclesServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	private VehicleLogic vehicleLogic;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
     public DisplayAllVehiclesServlet() {
         super();
+        vehicleLogic = new VehicleLogic();
         // TODO Auto-generated constructor stub
     }
 
@@ -24,8 +33,20 @@ public class DisplayAllVehiclesServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		try {
+			List<Vehicle> vehicles = vehicleLogic.displayAll();
+			if (vehicles != null) {
+				request.setAttribute("vehicles", vehicles);
+				request.getRequestDispatcher("./db/vehicle/DisplayAllVehiclesSuccessful.jsp").forward(request, response);
+			} else {
+				request.setAttribute("err", "Operation failed");
+				request.getRequestDispatcher("./db/vehicle/DisplayAllVehiclesFailed.jsp").forward(request, response);
+			}
+		}
+		catch (ClassNotFoundException | SQLException e) {
+			request.setAttribute("err", e.toString());
+			request.getRequestDispatcher("./DisplayAllVehiclesFailed.jsp").forward(request, response);
+		}
 	}
 
 	/**
