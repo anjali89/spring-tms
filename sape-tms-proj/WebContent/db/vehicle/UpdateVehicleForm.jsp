a01e0cf3ecc2f6a03db7cd00c84d5e6aaabe5db3
Yash Ugrankar <yash.ugrankar@gmail.com>
2017-03-05 05:57:45 +0530

    Complete Update Vehicle

---
+++ sape-tms-proj/WebContent/db/vehicle/UpdateVehicleForm.jsp
@@ -0,0 +1,19 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Update Vehicle Form</title>
+</head>
+<body>
+	<h1>Update Vehicle</h1>
+	<form action="${pageContext.request.contextPath}/UpdateVehicle" method="post">
+		ID: <input type="text" name="id" required /><br>
+		Model Name: <input type="text" name="modelName" required /><br>
+		Brand Name: <input type="text" name="brandName" required /><br>
+		Capacity: <input type="number" name="capacity" required /><br>
+		<input type="submit" value="Submit" />
+	</form>
+</body>
+</html>
--- sape-tms-proj/src/com/sapient/tms/controller/db/vehicle/UpdateVehicleServlet.java
+++ sape-tms-proj/src/com/sapient/tms/controller/db/vehicle/UpdateVehicleServlet.java
@@ -1,22 +1,32 @@
 package com.sapient.tms.controller.db.vehicle;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.sapient.tms.helper.VehicleData;
+import com.sapient.tms.model.bean.Vehicle;
+import com.sapient.tms.model.bl.VehicleLogic;
 
 /**
  * Servlet implementation class UpdateVehicleServlet
  */
 public class UpdateVehicleServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+	private VehicleLogic vehicleLogic;
+    private VehicleData vehicleData;
     /**
      * @see HttpServlet#HttpServlet()
      */
     public UpdateVehicleServlet() {
         super();
+        vehicleData = new VehicleData();
+        vehicleLogic = new VehicleLogic();
         // TODO Auto-generated constructor stub
     }
 
@@ -24,8 +34,24 @@ public class UpdateVehicleServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		try {
+			PrintWriter pw = response.getWriter();
+			boolean check = false;
+			String vehicleId = request.getParameter("id");
+			vehicleData.createVehicle(request);
+			Vehicle newVehicle = vehicleData.getVehicle();
+			check = vehicleLogic.update(vehicleId, newVehicle);
+			if (check) {
+				request.getRequestDispatcher("./db/vehicle/UpdateVehicleSuccessful.jsp").forward(request, response);
+			} else {
+				request.setAttribute("err", "Operation Failed");
+				request.getRequestDispatcher("./db/vehicle/UpdateVehicleFailed.jsp").forward(request, response);
+			}
+		}
+		catch (ClassNotFoundException | SQLException e) {
+			request.setAttribute("err", e.toString());
+			request.getRequestDispatcher("./UpdateVehicleFailed.jsp").forward(request, response);
+		}
 	}
 
 	/**
