0744d3c96e65194f6da70921a73d2c19eb9dcf2a
Yash Ugrankar <yash.ugrankar@gmail.com>
2017-03-05 13:35:12 +0530

    Complete DeleteVehicle

--- sape-tms-proj/WebContent/db/vehicle/DeleteVehicleForm.jsp
+++ sape-tms-proj/WebContent/db/vehicle/DeleteVehicleForm.jsp
@@ -7,8 +7,8 @@
 <title>Insert Vehicle Form</title>
 </head>
 <body>
-	<h1>Search Vehicle</h1>
-	<form action="${pageContext.request.contextPath}/SearchVehicle" method="post">
+	<h1>Delete Vehicle</h1>
+	<form action="${pageContext.request.contextPath}/DeleteVehicle" method="post">
 		ID: <input type="text" name="id" required /><br>
 		<input type="submit" value="Submit" />
 	</form>
--- sape-tms-proj/src/com/sapient/tms/controller/db/vehicle/DeleteVehicleServlet.java
+++ sape-tms-proj/src/com/sapient/tms/controller/db/vehicle/DeleteVehicleServlet.java
@@ -1,22 +1,30 @@
 package com.sapient.tms.controller.db.vehicle;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.sapient.tms.model.bean.Vehicle;
+import com.sapient.tms.model.bl.VehicleLogic;
 
 /**
  * Servlet implementation class DeleteVehicleServlet
  */
 public class DeleteVehicleServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	private VehicleLogic vehicleLogic;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
     public DeleteVehicleServlet() {
         super();
+        vehicleLogic = new VehicleLogic();
         // TODO Auto-generated constructor stub
     }
 
@@ -24,8 +32,21 @@ public class DeleteVehicleServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		try {
+			PrintWriter pw = response.getWriter();
+			String vehicleId = request.getParameter("id");
+			boolean success = vehicleLogic.delete(vehicleId);
+			if (success) {
+				request.getRequestDispatcher("./db/vehicle/DeleteVehicleSuccessful.jsp").forward(request, response);
+			} else {
+				request.setAttribute("err", "Entry Not Found");
+				request.getRequestDispatcher("./db/vehicle/DeleteVehicleFailed.jsp").forward(request, response);
+			}
+		}
+		catch (ClassNotFoundException | SQLException e) {
+			request.setAttribute("err", e.toString());
+			request.getRequestDispatcher("./DeleteVehicleFailed.jsp").forward(request, response);
+		}
 	}
 
 	/**
