f03452eeb8affcfad75eb632e5c9480a5253a838
agu237 <kkrtansh@gmail.com>
2017-03-06 11:09:22 -0800

    SearchRide Functionality

--- sape-tms-proj/src/com/sapient/tms/controller/db/ride/SearchRideServlet.java
+++ sape-tms-proj/src/com/sapient/tms/controller/db/ride/SearchRideServlet.java
@@ -1,37 +1,66 @@
 package com.sapient.tms.controller.db.ride;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.sapient.tms.model.bean.Employee;
+import com.sapient.tms.model.bean.Ride;
+import com.sapient.tms.model.bl.EmployeeLogic;
+import com.sapient.tms.model.bl.RideLogic;
 
 /**
  * Servlet implementation class SearchRideServlet
  */
 public class SearchRideServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public SearchRideServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+	private RideLogic rideLogic;
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	public SearchRideServlet() {
+		super();
+		rideLogic = new RideLogic();
+		// TODO Auto-generated constructor stub
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		try {
+			PrintWriter pw = response.getWriter();
+			String vehicleId = request.getParameter("id");
+			Ride ride = rideLogic.searchByVehicleId(vehicleId);
+			if (ride != null) {
+				request.setAttribute("ride", ride);
+				request.getRequestDispatcher("./WebContent/db/ride/SearchRideSuccessful.jsp").forward(request, response);
+			} else {
+				request.setAttribute("err", "Entry Not Found");
+				request.getRequestDispatcher("./WebContent/db/ride/SearchRideFailed.jsp").forward(request, response);
+			}
+		}
+
+		catch (ClassNotFoundException | SQLException e) {
+			request.setAttribute("err", e.toString());
+			request.getRequestDispatcher("./db/ride/SearchRideFailed.jsp").forward(request, response);
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
